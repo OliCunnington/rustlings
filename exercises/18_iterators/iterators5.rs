46ae44b4fb50ea3799fb99197d5b9c95b3741595
Oli <ofcunnington@hotmail.com>
2025-11-14 14:14:40 +0100

    18_iterators - incomplete

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    let (_, b) = map.values().into_iter().filter(|x| **x == value).size_hint();
+    b.unwrap() 
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,10 +50,12 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    collection.into_iter().map(|x| x.values().into_iter().filter(|y| **y == value).size_hint()).map(|(_, n)| n.unwrap()).sum()
 }
 
 fn main() {
     // You can optionally experiment here.
+    
 }
 
 #[cfg(test)]
